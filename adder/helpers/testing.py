cddfa62c07c6753c62659874f4f8205c91a70920
Stanley Gu <stanleygu@gmail.com>
2014-12-02 10:50:21 -0800

    Added adjustable delay in truth table tester

--- adder/helpers/testing.py
+++ adder/helpers/testing.py
@@ -1,8 +1,8 @@
 def test_truth_table(testmodel, input_ids, output_ids, truth_table,
-         ht=0.8, lt=0.2, plot=False):
+         ht=0.8, lt=0.2, delay=99, plot=False):
     import tellurium as te
     r = te.loada(testmodel)
-    
+
     sims = []
 
     for row in truth_table:
@@ -11,9 +11,8 @@ def test_truth_table(testmodel, input_ids, output_ids, truth_table,
             message.append(input_ids[i] + ':' + str(input_val))
             r[input_ids[i]] = input_val
 
-        sim = r.simulate(0, 100, 100, ['time'] + input_ids + output_ids)
+        sim = r.simulate(0, delay + 1, delay + 1, ['time'] + input_ids + output_ids)
         sims.append(sim)
-        t = 99
 
         for i, output_val in enumerate(row[1]):
             offset = len(input_ids) + 1 # Time + length of inputs
@@ -21,11 +20,11 @@ def test_truth_table(testmodel, input_ids, output_ids, truth_table,
             full_message = ' '.join(message + [
                 'then',
                 output_ids[i] + ':' + str(output_val),
-                '; Found %s = %f' % (output_ids[i], sim[t][ind])
+                '; Found %s = %f' % (output_ids[i], sim[delay][ind])
             ])
             print full_message
             if output_val == 0:
-                assert sim[t][ind] < lt, full_message
+                assert sim[delay][ind] < lt, full_message
             else:
-                assert sim[t][ind] > ht, full_message
+                assert sim[delay][ind] > ht, full_message
     return sims
